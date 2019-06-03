0fec30f833963709ae4737fc86fdb41ccea7b4d0
jacquelinevanarkel <43068350+jacquelinevanarkel@users.noreply.github.com>
2019-06-03 15:52:34 +0200

    Add files via upload

---
+++ BOW.py
@@ -0,0 +1,57 @@
+import numpy as np
+import argparse
+import re
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import svm
+
+#Opening files
+file = open("/users/Jacqueline/Documents/Language_Technology_Project/train_preprocessed.csv")
+content = file.readlines()
+file.close()
+data = [line.split(",") for line in content] 
+#Delete the unnecessary columns
+for x in data:
+    del x[1]
+    del x[4]
+
+#Create an list with all the comments and with all the classes (labels)
+comments = []
+classes = []
+for x in data[1:]:
+    y = x[0]
+    z = x[1:]
+    comments.append(y)
+    classes.append(z)
+
+#Change the label representation to 'glued' numbers (from list of "0.0 0.0 etc." to "00etc."")
+new = ""
+lab = []
+for labels in classes:
+    for label in labels:
+        label = int(float(label))
+        label = str(label)
+        new = new + label
+    lab.append(new)
+    new = ""
+
+#Vectorize the comments                
+vectorizer = CountVectorizer()
+vectors = vectorizer.fit_transform(comments)
+
+#Define train and test sets (10% test)
+x_train = vectors[:143615]
+x_test = vectors[143615:]
+y_train = lab[:143615]
+y_test = lab[143615:]
+
+#Train and test classifier
+clf = svm.SVC()
+clf.fit(x_train,y_train)
+score = clf.score(x_test, y_test)
+print ("Score:\n", score)
