896ce342c8c4b216ccd4eb7decc2fbeaaf814b49
arjun-006 <arjunharish006@gmail.com>
2019-06-07 13:25:44 +0200

    BERT tensorflow implementation

---
+++ tfrecordgenerator.py
@@ -0,0 +1,234 @@
+import collections
+
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import tensorflow as tf
+import tensorflow_hub as hub
+from datetime import datetime
+import bert
+from bert import run_classifier, run_classifier_with_tfhub
+from bert import optimization
+from bert import tokenization
+from bert import modeling
+import os
+
+path = '/home/arjun/PycharmProjects/BERT-toxic-classification'
+OUTPUT_DIR = os.path.join(path +'/output')
+train_data_path=os.path.join(path + '/data/toxic_comments/train.csv')
+print (train_data_path)
+train = pd.read_csv(train_data_path)
+test = pd.read_csv(os.path.join(path + '/data/toxic_comments/test.csv'))
+MAX_SEQ_LENGTH = 128
+
+BERT_VOCAB= os.path.join(path +'/data/toxic_comments/uncased_L-12_H-768_A-12/vocab.txt')
+BERT_INIT_CHKPNT = os.path.join(path +'/data/toxic_comments/uncased_L-12_H-768_A-12/bert_model.ckpt')
+BERT_CONFIG = os.path.join(path +'/data/toxic_comments/uncased_L-12_H-768_A-12/bert_config.json')
+
+tokenization.validate_case_matches_checkpoint(True,BERT_INIT_CHKPNT)
+tokenizer = tokenization.FullTokenizer(
+      vocab_file=BERT_VOCAB, do_lower_case=True)
+
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(self, input_ids, input_mask, segment_ids, label_ids, is_real_example=True):
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_ids = label_ids,
+        self.is_real_example=is_real_example
+
+
+class InputExample(object):
+    """A single training/test example for simple sequence classification."""
+
+    def __init__(self, guid, text_a, text_b=None, labels=None):
+        """Constructs a InputExample.
+
+        Args:
+            guid: Unique id for the example.
+            text_a: string. The untokenized text of the first sequence. For single
+            sequence tasks, only this sequence must be specified.
+            text_b: (Optional) string. The untokenized text of the second sequence.
+            Only must be specified for sequence pair tasks.
+            labels: (Optional) [string]. The label of the example. This should be
+            specified for train and dev examples, but not for test examples.
+        """
+        self.guid = guid
+        self.text_a = text_a
+        self.text_b = text_b
+        self.labels = labels
+
+def _truncate_seq_pair(tokens_a, tokens_b, max_length):
+    """Truncates a sequence pair in place to the maximum length."""
+
+    # This is a simple heuristic which will always truncate the longer sequence
+    # one token at a time. This makes more sense than truncating an equal percent
+    # of tokens from each, since if one sequence is very short then each token
+    # that's truncated likely contains more information than a longer sequence.
+    while True:
+        total_length = len(tokens_a) + len(tokens_b)
+        if total_length <= max_length:
+            break
+        if len(tokens_a) > len(tokens_b):
+            tokens_a.pop()
+        else:
+            tokens_b.pop()
+
+
+
+def convert_single_example(ex_index, example, max_seq_length,
+                           tokenizer):
+    """Converts a single `InputExample` into a single `InputFeatures`."""
+
+    # if isinstance(example, PaddingInputExample):
+    # return InputFeatures(
+    #     input_ids=[0] * max_seq_length,
+    #     input_mask=[0] * max_seq_length,
+    #     segment_ids=[0] * max_seq_length,
+    #     label_ids=0,
+    #     is_real_example=False)
+
+    tokens_a = tokenizer.tokenize(example.text_a)
+    tokens_b = None
+    if example.text_b:
+        tokens_b = tokenizer.tokenize(example.text_b)
+
+    if tokens_b:
+        # Modifies `tokens_a` and `tokens_b` in place so that the total
+        # length is less than the specified length.
+        # Account for [CLS], [SEP], [SEP] with "- 3"
+        _truncate_seq_pair(tokens_a, tokens_b, max_seq_length - 3)
+    else:
+        # Account for [CLS] and [SEP] with "- 2"
+        if len(tokens_a) > max_seq_length - 2:
+            tokens_a = tokens_a[0:(max_seq_length - 2)]
+
+    # The convention in BERT is:
+    # (a) For sequence pairs:
+    #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
+    #  type_ids: 0     0  0    0    0     0       0 0     1  1  1  1   1 1
+    # (b) For single sequences:
+    #  tokens:   [CLS] the dog is hairy . [SEP]
+    #  type_ids: 0     0   0   0  0     0 0
+    #
+    # Where "type_ids" are used to indicate whether this is the first
+    # sequence or the second sequence. The embedding vectors for `type=0` and
+    # `type=1` were learned during pre-training and are added to the wordpiece
+    # embedding vector (and position vector). This is not *strictly* necessary
+    # since the [SEP] token unambiguously separates the sequences, but it makes
+    # it easier for the model to learn the concept of sequences.
+    #
+    # For classification tasks, the first vector (corresponding to [CLS]) is
+    # used as the "sentence vector". Note that this only makes sense because
+    # the entire model is fine-tuned.
+    tokens = []
+    segment_ids = []
+    tokens.append("[CLS]")
+    segment_ids.append(0)
+    for token in tokens_a:
+        tokens.append(token)
+        segment_ids.append(0)
+    tokens.append("[SEP]")
+    segment_ids.append(0)
+
+    if tokens_b:
+        for token in tokens_b:
+            tokens.append(token)
+            segment_ids.append(1)
+        tokens.append("[SEP]")
+        segment_ids.append(1)
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    # The mask has 1 for real tokens and 0 for padding tokens. Only real
+    # tokens are attended to.
+    input_mask = [1] * len(input_ids)
+
+    # Zero-pad up to the sequence length.
+    while len(input_ids) < max_seq_length:
+        input_ids.append(0)
+        input_mask.append(0)
+        segment_ids.append(0)
+
+    assert len(input_ids) == max_seq_length
+    assert len(input_mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
+
+    labels_ids = []
+    for label in example.labels:
+        labels_ids.append(int(label))
+
+
+    feature = InputFeatures(
+        input_ids=input_ids,
+        input_mask=input_mask,
+        segment_ids=segment_ids,
+        label_ids=labels_ids,
+        is_real_example=True)
+    return feature
+
+def create_examples(df, labels_available=True):
+    """Creates examples for the training and dev sets."""
+    examples = []
+    for (i, row) in enumerate(df.values):
+        guid = row[0]
+        text_a = row[1]
+        if labels_available:
+            labels = row[2:]
+        else:
+            labels = [0,0,0,0,0,0]
+        examples.append(
+            InputExample(guid=guid, text_a=text_a, labels=labels))
+    return examples
+
+TRAIN_VAL_RATIO = 0.9
+LEN = train.shape[0]
+SIZE_TRAIN = int(TRAIN_VAL_RATIO*LEN)
+
+x_train = train[:SIZE_TRAIN]
+x_val = train[SIZE_TRAIN:]
+train_examples = create_examples(x_train)
+
+def file_based_convert_examples_to_features(
+        examples, max_seq_length, tokenizer, output_file):
+    """Convert a set of `InputExample`s to a TFRecord file."""
+
+    writer = tf.python_io.TFRecordWriter(output_file)
+
+    for (ex_index, example) in enumerate(examples):
+        #if ex_index % 10000 == 0:
+            #tf.logging.info("Writing example %d of %d" % (ex_index, len(examples)))
+
+        feature = convert_single_example(ex_index, example,
+                                         max_seq_length, tokenizer)
+
+        def create_int_feature(values):
+            f = tf.train.Feature(int64_list=tf.train.Int64List(value=list(values)))
+            return f
+
+        features = collections.OrderedDict()
+        features["input_ids"] = create_int_feature(feature.input_ids)
+        features["input_mask"] = create_int_feature(feature.input_mask)
+        features["segment_ids"] = create_int_feature(feature.segment_ids)
+        features["is_real_example"] = create_int_feature(
+            [int(feature.is_real_example)])
+        if isinstance(feature.label_ids, list):
+            label_ids = feature.label_ids
+        else:
+            label_ids = feature.label_ids[0]
+        features["label_ids"] = create_int_feature(label_ids)
+
+        tf_example = tf.train.Example(features=tf.train.Features(feature=features))
+        writer.write(tf_example.SerializeToString())
+    writer.close()
+
+
+#from pathlib import Path
+train_file = os.path.join(OUTPUT_DIR, "train.tf_record")
+#filename = Path(train_file)
+if not os.path.exists(train_file):
+    open(train_file, 'w').close()
+
+file_based_convert_examples_to_features(
+            train_examples, MAX_SEQ_LENGTH, tokenizer, train_file)
