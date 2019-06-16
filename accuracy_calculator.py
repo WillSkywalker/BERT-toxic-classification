bb6dded91e02ab862fba73a9c3d1d506c1a973a0
arjun-006 <arjunharish006@gmail.com>
2019-06-16 13:16:03 +0200

    BERT accuracy calculator with refined changes

--- accuracy_calculator.py
+++ accuracy_calculator.py
@@ -8,27 +8,29 @@ data = pd.read_csv("./results/results.csv")
 y_true = data.iloc[:,1:4]
 y_pred = data.iloc[:,5:8]
 print (data.iloc[0,5:8])
-# print (y_true)
-# print (mean_absolute_error(y_true.values.astype(float),y_pred.values.astype(float)))
-# print (np.array(data.values).shape[0])
 mae = []
 
+
+# Making the confusion matrix
 def merger(y):
     print(y,''.join(map(str, map(np.round, y))) )
     return ''.join(map(str, map(int, map(np.round, y))))
 
-
-# print (merger(np.round(y_pred.values.astype(np.double))))
-c_mat = confusion_matrix(list(map(merger, y_true.values)), list(map(merger, y_pred.values)),labels=['000','001','010','011','100','101','110','111'])
-# print(np.round(y_true.values.astype(np.double)))
+lab_order = ['000','001','010','011','100','101','110','111']
+c_mat = confusion_matrix(list(map(merger, y_true.values)), list(map(merger, y_pred.values)),labels=lab_order)
 print (c_mat)
-# df_cm = pd.DataFrame(c_mat, index = ['Toxic','Obscene','Insult'],
-#                   columns = ['Toxic_p','Obscene_p','Insult_p'])
 sn.set()
-# c_mat = c_mat.pivot('000','001','010','011','100','101','110','111')
-c_mat = pd.DataFrame(c_mat,columns=['000','001','010','011','100','101','110','111'])
-sn.heatmap(c_mat, annot=True, vmax=np.mean(c_mat.values),vmin=-5,xticklabels=['000','001','010','011','100','101','110','111'])
+fig, ax = plt.subplots(figsize=(15,10))
+ax = sn.heatmap(c_mat, annot=True, vmax=np.mean(c_mat)+60,vmin=-20,fmt='g')
+ax.set_title('BERT confusion matrix', fontsize=20, fontdict={})
+plt.yticks(np.arange(8)+0.5,lab_order,va="center",fontsize="20")
+plt.xticks(np.arange(8)+0.5,lab_order,va="center",fontsize="20")
+plt.ylabel("Gold Standard",fontsize="20")
+plt.xlabel("Predictions",fontsize="20")
 plt.show()
+
+
+# Calculating the accuracy and MAE
 accuracy = np.array([])
 for i in range(np.array(data.values).shape[0]):
     y_t = data.iloc[i,1:4]
